71431cdc91f19f9e084d93c043378a7082dfd960
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-08-10 12:47:10 +0600

    Complete Header, Tab buttons, Hero area

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,24 @@
-(function($){
-	$(document).ready(function() {	
-
+(function ($) {
+	$(document).ready(function () {
 		// Scroll to Top
-		jQuery('.scrolltotop').click(function(){
-			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+		jQuery(".scrolltotop").click(function () {
+			jQuery("html").animate({ scrollTop: "0px" }, 400);
 			return false;
 		});
-		
-		jQuery(window).scroll(function(){
+
+		jQuery(window).scroll(function () {
 			var upto = jQuery(window).scrollTop();
-			if(upto > 500) {
-				jQuery('.scrolltotop').fadeIn();
+			if (upto > 500) {
+				jQuery(".scrolltotop").fadeIn();
 			} else {
-				jQuery('.scrolltotop').fadeOut();
+				jQuery(".scrolltotop").fadeOut();
 			}
 		});
 
-
-		
-
-				
-		
-		
-		
-		
-		
-		
-		
-		
+		$(".mobile-search-icon i").click(function () {
+			$(".akbank-logo").toggleClass("d-none");
+			$(".full-width-search .search-box").toggleClass("d-none");
+			$(this).toggleClass("fa-solid, fa-xmark");
+		});
 	});
 })(jQuery);
